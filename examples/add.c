658ab4178c184d348db8c1eb99905e6e2f9e156f
AvirukBasak <avirukbasak@gmail.com>
2024-06-19 22:57:08 +0530

    Refactor add.c to improve error handling and simplify code

--- examples/add.c
+++ examples/add.c
@@ -6,11 +6,24 @@ rt_Data_t shsc_add(rt_Data_t context, rt_DataList_t *numbers) {
     for (int i = 0; i < rt_DataList_length(numbers); ++i) {
         char str[1024] = {0};
         sprintf(str, "number[%d]", i);
-        rt_Data_assert_type(
-            *rt_DataList_getref(numbers, i),
-            rt_DATA_TYPE_I64,
-            str
+
+        // complex, adds a stack frame for better error messages
+        rt_fn_call_handler(
+            rt_Data_null(),
+            "assert", "type",
+            rt_DataList_from(
+                *rt_DataList_getref(numbers, i),
+                rt_Data_i64(rt_DATA_TYPE_I64),
+                rt_Data_str(rt_DataStr_init(str))
+            )
         );
+
+        // alternative, simpler
+        // rt_Data_assert_type(
+        //     *rt_DataList_getref(numbers, i),
+        //     rt_DATA_TYPE_I64,
+        //     str
+        // );
     }
 
     int64_t sum = 0;
