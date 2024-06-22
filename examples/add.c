62e34dcea1f197ec959a6cfaa7c8df935884adb3
AvirukBasak <avirukbasak@gmail.com>
2024-06-22 19:19:16 +0530

    make us of context object this and print it

--- examples/add.c
+++ examples/add.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 #include "shsc.h"
 
-rt_Data_t shsc_add(rt_Data_t context, rt_DataList_t *numbers) {
+rt_Data_t shsc_add(rt_Data_t this, rt_DataList_t *args) {
     // validate input
-    for (int i = 0; i < shsc_rt_DataList_length(numbers); ++i) {
+    for (int i = 0; i < shsc_rt_DataList_length(args); ++i) {
         char str[1024] = {0};
         sprintf(str, "number[%d]", i);
 
@@ -12,7 +12,7 @@ rt_Data_t shsc_add(rt_Data_t context, rt_DataList_t *numbers) {
             shsc_rt_Data_null(),
             "assert", "type",
             shsc_rt_DataList_from(
-                *shsc_rt_DataList_getref(numbers, i),
+                *shsc_rt_DataList_getref(args, i),
                 shsc_rt_Data_i64(rt_DATA_TYPE_I64),
                 shsc_rt_Data_str(shsc_rt_DataStr_init(str))
             )
@@ -26,9 +26,20 @@ rt_Data_t shsc_add(rt_Data_t context, rt_DataList_t *numbers) {
         // );
     }
 
+    // print this
+    shsc_rt_fn_call_handler(
+        shsc_rt_Data_null(),
+        "io", "print",
+        shsc_rt_DataList_from(
+            shsc_rt_Data_str(shsc_rt_DataStr_init("this")),
+            this,
+            shsc_rt_Data_chr('\n')
+        )
+    );
+
     int64_t sum = 0;
-    for (int i = 0; i < shsc_rt_DataList_length(numbers); ++i) {
-        sum += shsc_rt_DataList_getref(numbers, i)->data.i64;
+    for (int i = 0; i < shsc_rt_DataList_length(args); ++i) {
+        sum += shsc_rt_DataList_getref(args, i)->data.i64;
     }
 
     return shsc_rt_Data_i64(sum);
