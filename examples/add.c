529d17739e1a145454aa794557fb7ffa8706e844
AvirukBasak <avirukbasak@gmail.com>
2024-07-16 03:05:55 +0530

    refactor shsc_add function to use updated API calls and improve readability

--- examples/add.c
+++ examples/add.c
@@ -1,46 +1,56 @@
 #include <stdio.h>
 #include "shsc.h"
+#include "shscproto.h"
 
 rt_Data_t shsc_add(rt_Data_t this, rt_DataList_t *args) {
     // validate input
-    for (int i = 0; i < shsc_rt_DataList_length(args); ++i) {
+    for (int i = 0; i < shsc_DataList_length(args); ++i) {
         char str[1024] = {0};
         sprintf(str, "number[%d]", i);
 
         // complex, adds a stack frame for better error messages
-        shsc_rt_fn_call_handler(
-            shsc_rt_Data_null(),
+        shsc_call_fn(
+            shsc_Data_null(),
             "assert", "type",
-            shsc_rt_DataList_from(
-                *shsc_rt_DataList_getref(args, i),
-                shsc_rt_Data_i64(rt_DATA_TYPE_I64),
-                shsc_rt_Data_str(shsc_rt_DataStr_init(str))
+            shsc_DataList_from(
+                *shsc_DataList_getref(args, i),
+                shsc_Data_i64(rt_DATA_TYPE_I64),
+                shsc_Data_str(shsc_DataStr_init(str))
             )
         );
 
         // alternative, simpler
-        // rt_Data_assert_type(
-        //     *rt_DataList_getref(numbers, i),
+        // shsc_Data_assert_type(
+        //     *shsc_DataList_getref(numbers, i),
         //     rt_DATA_TYPE_I64,
         //     str
         // );
     }
 
+    rt_Data_t file = shsc_call_fn(
+        shsc_Data_null(),
+        "dbg", "filename",
+        shsc_DataList_from()
+    );
+
     // print this
-    shsc_rt_fn_call_handler(
-        shsc_rt_Data_null(),
+    shsc_call_fn(
+        shsc_Data_null(),
         "io", "print",
-        shsc_rt_DataList_from(
-            shsc_rt_Data_str(shsc_rt_DataStr_init("this")),
+        shsc_DataList_from(
+            file,
+            shsc_Data_str(shsc_DataStr_init("this")),
             this,
-            shsc_rt_Data_chr('\n')
+            shsc_Data_chr('\n')
         )
     );
 
+    // shsc_Data_destroy(&file);
+
     int64_t sum = 0;
-    for (int i = 0; i < shsc_rt_DataList_length(args); ++i) {
-        sum += shsc_rt_DataList_getref(args, i)->data.i64;
+    for (int i = 0; i < shsc_DataList_length(args); ++i) {
+        sum += shsc_DataList_getref(args, i)->data.i64;
     }
 
-    return shsc_rt_Data_i64(sum);
+    return shsc_Data_i64(sum);
 }
